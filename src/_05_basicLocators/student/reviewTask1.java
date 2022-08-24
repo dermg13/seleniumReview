df420a5c31318472a2f41a569ef8ddca95141aa9
dermg13 <dermg13@gmail.com>
2022-08-24 18:46:21 -0400

    saving changes

---
+++ src/_05_basicLocators/student/reviewTask1.java
@@ -0,0 +1,11 @@
+package _05_basicLocators.student;
+
+public class reviewTask1 {
+    public static void main(String[] args) {
+        /**
+         * 1. Open etsy.com
+         * 2. Locate web elements using each locator
+         * 3. Locate same web elements using css selector
+         */
+    }
+}
