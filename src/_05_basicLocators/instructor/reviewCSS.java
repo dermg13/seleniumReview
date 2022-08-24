436ee5e7398f4c952732a109235ec097769c2b70
dermg13 <dermg13@gmail.com>
2022-08-24 19:56:49 -0400

    updated notes

--- src/_05_basicLocators/instructor/reviewCSS.java
+++ src/_05_basicLocators/instructor/reviewCSS.java
@@ -1,27 +1,69 @@
 package _05_basicLocators.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class reviewCSS {
     public static void main(String[] args) {
+
+//        tagAndId();
+        tagAndClass();
+
+        /**
+         * What are the types of CSS Selectors?
+         * 1. tag + id
+         * 2. tag + className
+         * 3. tag + attribute
+         */
+    }
+    public static void tagAndId(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        /**
-         * What are the types of CSS Selectors?
-         *
-         *
-         *
-         */
-    }
-    public static void tagAndId(){
+        driver.get("https://www.etsy.com/");
 
+        //By id in css selector
+        driver.findElement(By.id("activity-checker")).isDisplayed();
+
+        //1. tag + id(#)
+        System.out.println(driver.findElement(By.cssSelector("div#activity-checker")).getText());
+
+        //2. tag + attribute
+        System.out.println(driver.findElement(By.cssSelector("div[id='activity-checker']")).getText());
+
+        driver.close();
     }
 
     public static void tagAndClass(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
+        driver.get("https://www.etsy.com/");
+
+        //By class in css selector
+        System.out.println(driver.findElement(By.className("currency-value")).getText());
+
+        //1. tag + class(.)
+        System.out.println("second way " + driver.findElement(By.cssSelector(".currency-value")).getText());
+
+        //2. tag + attribute
+        List<WebElement> buttons = driver.findElements(By.cssSelector("p[class='wt-text-title-01 wt-text-center-xs wt-text-truncate--multi-line wt-text-black query-with-image-text wt-mb-xs-1 wt-hide-xs wt-show-lg']"));
+        System.out.println(buttons.size());
+
+        for(WebElement each : buttons) {
+            System.out.println(each.getText());
+        }
+        System.out.println("-----Print only Clothing-----");
+        System.out.println(buttons.get(2).getText());
+
+
+        driver.close();
     }
     public static void tadAndAttribute(){
 
--- src/_05_basicLocators/instructor/reviewLocators.java
+++ src/_05_basicLocators/instructor/reviewLocators.java
@@ -1,5 +1,6 @@
 package _05_basicLocators.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +12,33 @@ public class reviewLocators {
 
         /**
          * What are the selenium locators?
-         *
+         * 1. id  - Unique
+         * 2. name
+         * 3. className
+         * 4. tagName
+         * 5. linkText
+         * 6. partialLinkText
+         * 7. css
+         * 8. xpath
          */
+
+        driver.get("https://www.etsy.com/");
+
+        //using id -- this has no text
+        System.out.println(driver.findElement(By.id("google-one-tap-modal-div")).getText());
+
+        // using className
+        System.out.println(driver.findElement(By.className("wt-display-table-cell")).getText());
+
+        //using tagName
+        System.out.println(driver.findElement(By.tagName("span")).getText());
+
+        //using linkText
+        System.out.println(driver.findElement(By.linkText("Personalized Gifts")).getText());
+
+        //using partialLinkText
+        System.out.println(driver.findElement(By.partialLinkText("Personalized")).getText());
+
+        driver.close();
     }
 }
