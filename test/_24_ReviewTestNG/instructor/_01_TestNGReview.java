ca47e4c10ee93e99b64353ec3faec2f5f5b66d4f
dermg13 <dermg13@gmail.com>
2022-09-28 23:18:08 -0400

    testNg Review

---
+++ test/_24_ReviewTestNG/instructor/_01_TestNGReview.java
@@ -0,0 +1,47 @@
+package _24_ReviewTestNG.instructor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class _01_TestNGReview {
+
+    /**
+     * 1. @Test - Verify title of page
+     * 2. @Test (testName = "US001_Verify_Header")
+     * 3. @Test (testName = "US002_Verify_Button", description = "Verify button is enabled")
+     * 4. @Test (testName = "US003_Verify_subHeader", description = "Verify text is displayed", enabled = false)
+     * 5. priority
+     * 6. dataProvider
+     */
+
+    @Test
+    public void testWomenStore(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=categorywww.cars.com/");
+        Assert.assertEquals(driver.getTitle(), "Women");
+
+        driver.close();
+    }
+
+    @Test
+    public void testButton(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mder\\OneDrive\\Documents\\Selenium\\Drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=categorywww.cars.com/");
+        Assert.assertTrue(driver.findElement(By.xpath("//a[@class='sf-with-ul' and text()='Women']")).isEnabled());
+
+        driver.close();
+    }
+}
---
+++ test/_24_ReviewTestNG/student/notes.java
@@ -0,0 +1,5 @@
+package _24_ReviewTestNG.student;
+
+public class notes {
+    //Use this class to follow along
+}
